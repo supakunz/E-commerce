af86f21ac096a2b451cfa98e6b923af1e491b078
Supakun Thata <supakunt.thata@gmail.com>
2024-12-30 11:54:26 +0700

    fixed update controller

--- servers/controllers/users.js
+++ servers/controllers/users.js
@@ -9,7 +9,7 @@ const getAllUsers = async (req, res) => {
   } catch (error) {
     res.status(404).json({ message: error });
   }
-}
+};
 
 const createUsers = async (req, res) => {
   try {
@@ -20,7 +20,7 @@ const createUsers = async (req, res) => {
         .status(400)
         .json({ suscess: false, errors: "Duplicate email address." });
     }
-    
+
     const user = new UserSchema({
       // กำหนกค่าลงใน Table
       name: req.body.username,
@@ -40,50 +40,52 @@ const createUsers = async (req, res) => {
     const token = jwt.sign(data, "secret_ecom", { expiresIn: "1h" });
     res.json({ success: true, token });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.json({ success: "Failed Registering.", status: error });
   }
-}
+};
 
 const updateUsers = async (req, res) => {
   try {
-    const { name, email, password, role } = req.body
-    const { id } = req.params
+    const { name, email, password, role } = req.body;
+    const { id } = req.params;
 
     const data = {
       name,
       email,
       role,
-    }
+    };
 
     if (password) {
-      data.password = response.password
+      data.password = password;
     }
 
-    const updated = await UserSchema.findOneAndUpdate({ _id: id }, data, { new: true })
-    console.log(updated)
-    res.json({message:"Update User Successfully"})
+    const updated = await UserSchema.findOneAndUpdate({ _id: id }, data, {
+      new: true,
+    });
+    console.log(updated);
+    res.json({ message: "Update User Successfully" });
   } catch (error) {
-   res.status(404).json({message:error}) 
+    res.status(404).json({ message: error });
   }
-}
+};
 
 const deleteUsers = async (req, res) => {
   await UserSchema.findOneAndDelete({ _id: req.body.id });
   console.log("Remove Users");
   res.json({ success: true, name: req.body.name });
-}
+};
 
 const getUsersID = async (req, res) => {
   try {
-    const { id } = req.params
-    let users = await UserSchema.find({_id:id});
+    const { id } = req.params;
+    let users = await UserSchema.find({ _id: id });
     console.log("UsersID Fetched");
     res.send(users);
   } catch (error) {
     res.status(404).json({ message: error });
   }
-}
+};
 
 const loginUsers = async (req, res) => {
   let user = await UserSchema.findOne({ email: req.body.email });
@@ -108,7 +110,7 @@ const loginUsers = async (req, res) => {
   } else {
     res.json({ suscess: false, errors: "Invalid email please try again." });
   }
-}
+};
 
 module.exports = {
   getAllUsers,
@@ -116,5 +118,5 @@ module.exports = {
   createUsers,
   updateUsers,
   deleteUsers,
-  loginUsers
-}
+  loginUsers,
+};
